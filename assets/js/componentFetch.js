65e8912aafca720a0c85147dd8d849dd269104e8
Mikael Wellfine <103266482+wellfinee@users.noreply.github.com>
2025-02-07 23:24:14 +0500

    Update componentFetch.js

--- assets/js/componentFetch.js
+++ assets/js/componentFetch.js
@@ -1,9 +1,9 @@
-fetch('./../../components/header/header.html')
+fetch('https://wellfinee.github.io/tz-news/components/header/header.html')
     .then(response => response.text())
     .then(data => document.getElementById('header').innerHTML = data);
-fetch('./../../components/footer/footer.html')
+fetch('https://wellfinee.github.io/tz-news/components/footer/footer.html')
     .then(response => response.text())
     .then(data => document.getElementById('footer').innerHTML = data);
-fetch('./../../components/modal/modal.html')
+fetch('https://wellfinee.github.io/tz-news/components/modal/modal.html')
     .then(response => response.text())
     .then(data => document.getElementById('modal').innerHTML = data);
